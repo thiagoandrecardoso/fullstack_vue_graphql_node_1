2c55f6f39bcf5ce15743af10f86dded1628d0e9b
Thiago <thiagoandrecardoso@icloud.com>
2019-12-12 10:50:00 -0300

    Iniciado projeto FullStak - vue e node js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <div d="slogan" class="text-center">
+            <h1>NameGator</h1>
+            <br/>
+            <h6 class="text-secondary">Gerador de nomes, utilizando Vue,js, GraphQL e Node</h6>
+        </div>
+        <div id="main">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md">
+                        <h5>Prefixo <span class="badge badge-info">{{prefixes.length}}</span></h5>
+                        <div class="card">
+                            <div class="card-body">
+                                <ul class="list-group">
+                                    <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
+                                        {{prefix}}
+                                    </li>
+                                </ul>
+                                <br/>
+                                <input class="form-control" type="text" placeholder="Digite o prefixo"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md">
+                        <h5>Sufixo <span class="badge badge-info">{{sufixes.length}}</span></h5>
+                        <div class="card">
+                            <div class="card-body">
+                                <ul class="list-group">
+                                    <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
+                                        {{sufix}}
+                                    </li>
+                                </ul>
+                                <br/>
+                                <input class="form-control" type="text" placeholder="Digite o sufixo"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br/>
+            <h5>Dominios <span class="badge badge-info">{{domains.length}}</span></h5>
+            <div class="card">
+                <div class="card-body">
+                    <li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
+                        {{ domain }}
+                    </li>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import "bootstrap/dist/css/bootstrap.css"
+    import "font-awesome/css/font-awesome.css"
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        data: function () {
+            return {
+                prefixes: ['Air', 'Jet', 'Flight'],
+                sufixes: ['Hub', 'Station', 'Mart'],
+                domains: ['AirHub', 'AirStation', 'AirMart']
+            };
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #slogan {
+        margin-top: 30px;
+        margin-bottom: 30px;
+    }
+
+    #main {
+        background-color: #F1F1F1;
+        padding-top: 30px;
+        peeding-bottam: 20px;
+    }
 </style>
