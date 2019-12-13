1ac77cb6cb74f7c23646acc1828d7d9ef65236e4
Thiago André Cardoso Silva <thiagoandrecardoso@icloud.com>
2019-12-12 23:42:28 -0300

    Generate and create
    Interações com a lista feitas com Create e generate
    Sendo chamada o Generate em todos os metodos

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
             <br/>
             <h6 class="text-secondary">Gerador de nomes, utilizando Vue,js, GraphQL e Node</h6>
         </div>
-        <div id="smain">
+        <div id="main">
             <div class="container">
                 <div class="row">
                     <div class="col-md">
@@ -14,14 +14,23 @@
                             <div class="card-body">
                                 <ul class="list-group">
                                     <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
-                                        {{prefix}}
+                                        <div class="row">
+                                            <div class="col-md">
+                                                {{prefix}}
+                                            </div>
+                                            <div class="col-md text-right">
+                                                <button class="btn btn-info" v-on:click="deletePrefix(sufix)"><span class="fa fa-trash"></span></button>
+                                            </div>
+                                        </div>
                                     </li>
                                 </ul>
                                 <br/>
                                 <div class="input-group">
-                                    <input class="form-control" type="text" v-model="prefix" placeholder="Digite o prefixo"/>
+                                    <input class="form-control" type="text" v-model="prefix"
+                                           v-on:keyup.enter="addPrefix(prefix)" placeholder="Digite o prefixo"/>
                                     <div class="input-group-append">
-                                        <button class="btn btn-info" v-on:click="addPrefix(prefix)"><span class="fa fa-plus"></span></button>
+                                        <button class="btn btn-info" v-on:click="addPrefix(prefix)"><span
+                                                class="fa fa-plus"></span></button>
                                     </div>
                                 </div>
                             </div>
@@ -33,14 +42,23 @@
                             <div class="card-body">
                                 <ul class="list-group">
                                     <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
-                                        {{sufix}}
+                                        <div class="row">
+                                            <div class="col-md">
+                                                {{sufix}}
+                                            </div>
+                                            <div class="col-md text-right">
+                                                <button class="btn btn-info" v-on:click="deleteSufix(sufix)"><span class="fa fa-trash"></span></button>
+                                            </div>
+                                        </div>
                                     </li>
                                 </ul>
                                 <br/>
                                 <div class="input-group">
-                                    <input class="form-control" type="text" v-model="sufix" placeholder="Digite o sufixo"/>
+                                    <input class="form-control" type="text" v-model="sufix"
+                                           v-on:keyup.enter="addSufix(sufix)" placeholder="Digite o sufixo"/>
                                     <div class="input-group-append">
-                                        <button class="btn btn-info" v-on:click="addSufix(sufix)"> <span class="fa fa-plus"></span></button>
+                                        <button class="btn btn-info" v-on:click="addSufix(sufix)"><span
+                                                class="fa fa-plus"></span></button>
                                     </div>
                                 </div>
                             </div>
@@ -73,18 +91,39 @@
                 sufix: "",
                 prefixes: ['Coisa', 'Lira'],
                 sufixes: ['nte', 'ntson'],
-                domains: ['Lirante', 'Coisantson', 'Lirantson']
+                domains: []
             };
         },
         methods: {
-            addPrefix(prefix){
+            addPrefix(prefix) {
                 this.prefixes.push(prefix);
                 this.prefix = "";
+                this.generate();
             },
-            addSufix(sufix){
+            deletePrefix(prefix){
+                this.prefixes.splice(this.prefixes.indexOf(prefix), 1);
+                this.generate();
+            },
+            addSufix(sufix) {
                 this.sufixes.push(sufix);
                 this.sufix = ""
+                this.generate();
+            },
+            deleteSufix(sufix){
+                this.sufixes.splice(this.sufixes.indexOf(sufix), 1);
+                this.generate();
+            },
+            generate(){
+                this.domains = [];
+                for (const prefix of this.prefixes){
+                    for (const sufix of this.sufixes){
+                        this.domains.push(prefix + sufix);
+                    }
+                }
             }
+        },
+        created(){
+            this.generate();
         }
     }
 </script>
@@ -95,7 +134,7 @@
         margin-bottom: 30px;
     }
 
-    #smain {
+    #main {
         background-color: #F1F1F1;
         padding-top: 30px;
         peeding-bottam: 20px;
